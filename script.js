6e25552d8efc02001d2c4707b472f0da2345acb1
wjkierka <wjkierka@gmail.com>
2025-12-03 19:17:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿// Kod ten jest wykonywany w przeglądarce użytkownika
+document.getElementById('main-button').addEventListener('click', function() {
+    let output = document.getElementById('output-message');
+    let clicks = parseInt(output.dataset.clicks || 0) + 1; // Zliczanie kliknięć
+
+
+    output.textContent = "Kliknięto przycisk " + clicks + " razy!";
+    output.dataset.clicks = clicks;
+});
